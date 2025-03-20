6815d222c324db513ea275ff9dfc6e4113fc4682
hmikho <hanna.mikho@chasacademy.com>
2025-03-20 11:29:21 +0100

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/ReactCvPortal/", // Viktigt för att CSS och JS ska hittas på GitHub Pages
+  base: "/ReactCvPortal/",
   plugins: [react()],
 });
